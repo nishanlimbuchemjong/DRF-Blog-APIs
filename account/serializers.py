ca5d90d80d5321425dd79dae951f50ad2fce7a7b
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-15 18:35:20 +0545

    new user Registeration successfully

--- account/serializers.py
+++ account/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
-class RegisterSerializer(serializer.Serializer):
+class RegisterSerializer(serializers.Serializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
     username = serializers.CharField()
     password = serializers.CharField()
 
     def validate(self, data):
-        if User.objects.filter(username=data['username']).exist():
+        if User.objects.filter(username=data['username']).exists():
             raise serializers.ValidationError("username already taken.")
 
         return data
+    
+    def create(self, validated_data):
+        user = User.objects.create(first_name=validated_data['first_name'],
+                                   last_name=validated_data['last_name'],
+                                   username=validated_data['username'].lower())
+        user.set_password(validated_data['password'])
+        return validated_data
--- account/views.py
+++ account/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+from rest_framework import status
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+
+            serializer = RegisterSerializer(data=data)
+            if not serializer.is_valid():
+                return Response({
+                    'data': serializer.errors,
+                    'message': 'something went wrong'
+                }, status = status.HTTP_400_BAD_REQUEST)
+
+            serializer.save()
+            return Response({
+                'data': {},
+                'message': 'your account created successfully',
+            }, status = status.HTTP_201_CREATED)
+        except Exception as e:
+            print(e)
+            return Response({
+                'data': {},
+                'message': 'something went wrong'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
