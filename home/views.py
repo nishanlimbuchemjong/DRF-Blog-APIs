aa312f83e324089f9693c67c7a0425bb15c6a15c
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-15 20:06:59 +0545

    new blog created successfully after adding authentication

--- home/views.py
+++ home/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import BlogSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class BlogView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    def post(self, request):
+        try:
+            data = request.data
+            data['user'] = request.user.id
+
+            serializer = BlogSerializer(data=data)
+            if not serializer.is_valid():
+                return Response({
+                    'data': serializer.errors,
+                    'message': 'something went wrong'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response({
+                'data': serializer.data,
+                'message': 'blog created successfully',
+            }, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            print(e)
+            return Response({
+                'data': {},
+                'message': 'something went wrong'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+
